18bbd296397f9f311a2d3aec562929e7d3547040
Tim Abell <tim@timwise.co.uk>
2018-07-16 12:45:56 +0100

    Add context strings

--- LinqWut/LinqWutExtensions.cs
+++ LinqWut/LinqWutExtensions.cs
@@ -8,15 +8,20 @@ namespace LinqWut
     {
         public static TEntity SingleWithContext<TEntity>(this IEnumerable<TEntity> value)
         {
+            return value.SingleWithContext("");
+        }
+
+        public static TEntity SingleWithContext<TEntity>(this IEnumerable<TEntity> value, string context)
+        {
             var results = value.ToList();
             var resultsCount = results.Count;
             if (resultsCount == 0)
             {
-                throw new InvalidOperationException($"Expected 1 {typeof(TEntity)} but got 0");
+                throw new InvalidOperationException($"Expected 1 {typeof(TEntity)} but got 0 {context}".TrimEnd());
             }
             if (resultsCount > 1)
             {
-                throw new InvalidOperationException($"Expected 1 {typeof(TEntity)} but got {resultsCount}");
+                throw new InvalidOperationException($"Expected 1 {typeof(TEntity)} but got {resultsCount} {context}".TrimEnd());
             }
             return results[0];
         }
--- LinqWutTests/LinqWutTests.cs
+++ LinqWutTests/LinqWutTests.cs
@@ -33,5 +33,24 @@ namespace LinqWutTests
             var result = list.SingleWithContext();
             result.Should().Be(someVal);
         }
+
+        [TestMethod]
+        public void TestSingle_When_Multiple_WithContext()
+        {
+            var interestingListSource = new List<int> { 1, 2, 3 };
+            string context = $"when looking in {nameof(interestingListSource)}";
+            interestingListSource.Invoking(x => x.SingleWithContext(context))
+                .Should().Throw<InvalidOperationException>().WithMessage("Expected 1 System.Int32 but got 3 " + context);
+        }
+
+        [TestMethod]
+        public void TestSingle_When_None_WithContext()
+        {
+            var someEmptyList = new List<int>();
+            string context = $"when looking in {nameof(someEmptyList)}";
+            someEmptyList.Invoking(x => x.SingleWithContext(context))
+                .Should().Throw<InvalidOperationException>().WithMessage("Expected 1 System.Int32 but got 0 " + context);
+        }
+
     }
 }
